e9704d6b551bd0da7ee2a60ad4efc4f6404049d0
Sohaebabusaif <bodamaka52@gmail.com>
2026-08-28 18:57:51 +0300

    Increase body-parser limit to 50mb for base64 logos

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname))); // Serve frontend files
 
 // Setup Database
